b9d96d740a897882a96484b8faa3f96f3c8ef867
deryanurcaman <camanderya@gmail.com>
2021-05-24 20:18:47 +0300

    course_id kaldırılıp öğrenciler yeniden eklendi

--- insertStudents.php
+++ insertStudents.php
@@ -7,13 +7,23 @@ if(!$conn){
 
 
 
-$sql = "INSERT INTO students (student_id, first_name, last_name, gpa, class, course_id, username, password) 
-VALUES ('8101086', 'Colin', 'Creevey', '2.68', '1','2', 'colinc', '12345'),
-('8101087','Seamus', 'Finnigan','2.37', '2','2', 'seafin', '12345'),
-('8101088','Hannah', 'Abbott', '3.25', '4','2', 'hannahabb', '12345'),
-('8101089','Pansy', 'Parkinson', '2.97', '3','2', 'pansypar', '12345'),
-('8101090','Blaise', 'Zabini', '2.89', '1','2', 'blaiz', '12345'),
-('8101091','Zacharias', 'Smith', '3.28', '2','2', 'zsmith', '12345');";
+$sql = "INSERT INTO students (student_id, first_name, last_name, gpa, class, username, password) 
+VALUES ('8101086', 'Colin', 'Creevey', '2.68', '1', 'colinc', '12345'),
+('8101087','Seamus', 'Finnigan','2.37', '2', 'seafin', '12345'),
+('8101088','Hannah', 'Abbott', '3.25', '4', 'hannahabb', '12345'),
+('8101089','Pansy', 'Parkinson', '2.97', '3', 'pansypar', '12345'),
+('8101090','Blaise', 'Zabini', '2.89', '1', 'blaiz', '12345'),
+('8101091','Zacharias', 'Smith', '3.28', '2', 'zsmith', '12345');";
+
+
+// $sql = "INSERT INTO students (student_id, first_name, last_name, gpa, class, course_id, username, password) 
+// VALUES ('8101086', 'Colin', 'Creevey', '2.68', '1',(SELECT id FROM courses WHERE code = 'POT78055'), 'colinc', '12345'),
+// ('8101087','Seamus', 'Finnigan','2.37', '2',(SELECT id FROM courses WHERE code = 'POT78055'), 'seafin', '12345'),
+// ('8101088','Hannah', 'Abbott', '3.25', '4',(SELECT id FROM courses WHERE code = 'DDA95832'), 'hannahabb', '12345'),
+// ('8101089','Pansy', 'Parkinson', '2.97', '3',(SELECT id FROM courses WHERE code = 'DDA95832'), 'pansypar', '12345'),
+// ('8101090','Blaise', 'Zabini', '2.89', '1',(SELECT id FROM courses WHERE code = 'HRB61429'), 'blaiz', '12345'),
+// ('8101091','Zacharias', 'Smith', '3.28', '2',(SELECT id FROM courses WHERE code = 'HIS89407'), 'zsmith', '12345');";
+
 
 
 if (mysqli_query($conn, $sql)) {
