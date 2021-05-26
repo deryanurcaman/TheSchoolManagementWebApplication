d829c3942d61b018c2f6575857425e68486d4696
deryanurcaman <camanderya@gmail.com>
2021-05-26 13:44:45 +0300

    fix

--- insertCourses.php
+++ insertCourses.php
@@ -32,7 +32,7 @@ VALUES
 ('POT78055', 'Potions', 'Mandatory', 'Monday', CAST('09:00' AS time), CAST('11:00' AS time), (SELECT id FROM instructors WHERE instructor_code = '340003')),
 ('HIS89407', 'History of Magic', 'Mandatory', 'Tuesday', CAST('12:00' AS time), CAST('14:00' AS time), (SELECT id FROM instructors WHERE instructor_code = '340008')),
 ('CMC46702', 'Care of Magical Creatures', 'Elective', 'Tuesday', CAST('08:00' AS time), CAST('10:00' AS time), (SELECT id FROM instructors WHERE instructor_code = '340006')),
-('DDA95832', 'Defence Against the Dark Arts', 'Friday', 'Mandatory', CAST('14:00' AS time), CAST('16:00' AS time), (SELECT id FROM instructors WHERE instructor_code = '340008')),
+('DDA95832', 'Defence Against the Dark Arts', 'Mandatory','Friday', CAST('14:00' AS time), CAST('16:00' AS time), (SELECT id FROM instructors WHERE instructor_code = '340008')),
 ('FLY66582', 'Flying', 'Mandatory', 'Wednesday', CAST('13:00' AS time), CAST('15:00' AS time), (SELECT id FROM instructors WHERE instructor_code = '340005')),
 ('HRB61429', 'Herbology', 'Mandatory', 'Monday', CAST('13:00' AS time), CAST('15:00' AS time), (SELECT id FROM instructors WHERE instructor_code = '340003')),
 ('SAR46609', 'Study of Ancient Runes', 'Elective', 'Friday', CAST('17:00' AS time), CAST('19:00' AS time), (SELECT id FROM instructors WHERE instructor_code = '340007')),
